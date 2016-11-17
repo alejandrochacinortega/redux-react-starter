b2feeb5e6f15ebdd34ae51a4193e359519c310b2
omar.chacin <omar.chacin@sigmagroup.no>
2016-11-17 15:19:13 +0100

    add video list, item and detail components

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,23 @@
-/**
- * Created by omar on 17/11/16.
- */
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  const imageUrl =  video.snippet.thumbnails.default.url;
+  const title = video.snippet.title;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+            {title}
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+};
+
+export default VideoListItem
--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 
 const API_KEY = 'AIzaSyCo_o7jNDTEN7hlt8UNY0FIfPDAfULTUec';
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-};
 
-//Take this component's generated HTML and put it on the page (in the DOM)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+;
+
 ReactDOM.render(<App />, document.querySelector('.container'));
